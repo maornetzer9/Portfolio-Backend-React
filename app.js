d1ba1c1752acac3b17b2c6689334660990d40f8a
maornetzer9 <maorgithub@gmail.com>
2024-04-06 21:25:36 +0300

    # Fix Origin Path To Production

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const bodyParser = require('body-parser');
 const { nodemailerRouter } = require('./routes/nodemailer');
 
 const PORT = process.env.PORT;
-const ORIGIN = process.env.ORIGIN;
+const PRODUCTION = process.env.PRODUCTION;
 
 const app = express();
-const corsOptions = { origin: ORIGIN }
+const corsOptions = { origin: PRODUCTION }
 
 app.use( express.json() );
 app.use( nodemailerRouter )
 app.use( cors( corsOptions ) );
 app.use( bodyParser.urlencoded({ extended: false }) );
 
+app.get('/', async (req, res) => {
+    try
+    {
+        return res.status(200).json('Backend Running...');
+    }
+    catch(err)
+    {
+        console.error(err);
+        return res.status(500).json('Internal Server Error...');
+    }
+});
 
 app.listen(PORT, () => console.info(`Server is running on PORT: ${PORT}`));
